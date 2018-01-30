ee14d3ef00c0a96657c2bc68c3bce9c332f68af0
Natesh Kukreja <natesh.kukreja@gmail.com>
2018-01-30 12:11:04 -0500

    Create day0.py

---
+++ Python/10 Days of Statistics/day0.py
@@ -0,0 +1,47 @@
+from statistics import mode
+
+def mean(arrSize, arr):
+    sum = 0
+    for i in arr:
+        sum+=int(i)
+    print(sum/int(arrSize))
+    
+def median(arrSize, arr):
+    size = int(arrSize)
+    if(size%2 == 0):
+        var1 = (arr[int(size/2)])
+        var2 = (arr[int(size/2)-1])
+        print((var1+var2)/2)
+    else:
+        print((arr[size/2]))
+    
+def mode(lis):
+    dictionary = {}
+    lis.sort()
+    for val in lis:
+        dictionary[val] = 0
+    
+    for val in lis:
+        if(dictionary[val] >=0):
+            dictionary[val] += 1
+        
+        else:
+            dictionary[val] = 1
+    sorted(dictionary.keys())
+    count = 0
+    index = 0
+    for val in lis:
+        if dictionary[val]>count:
+            count = dictionary[val]
+            index = val
+    print(index)
+
+size = input()
+array = input().split()
+lis = []
+for a in array:
+    lis.append(int(a))
+lis.sort()
+mean(size, lis)
+median(size, lis)
+(mode(lis))
